b5b3b8b76c6ceaa35b1fc624b9082f9038bba0a0
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2021-11-18 18:28:43 +0100

    Rename the project name to MySoothe
    Rename the project name to MySoothe to differentiate the different
    projects of the Android Dev Challenge.

--- app/src/main/java/com/androiddevchallenge/week3/emea/MainActivity.kt
+++ app/src/main/java/com/androiddevchallenge/week3/emea/MainActivity.kt
@@ -23,14 +23,14 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.androiddevchallenge.week3.emea.ui.theme.MyTheme
+import com.androiddevchallenge.week3.emea.ui.theme.MySootheTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyTheme {
-                MyApp()
+            MySootheTheme {
+                MySootheApp()
             }
         }
     }
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity() {
 
 // Start building your app here!
 @Composable
-fun MyApp() {
+fun MySootheApp() {
     Surface(color = MaterialTheme.colors.background) {
         Text(text = "Ready... Set... GO!")
     }
@@ -47,15 +47,15 @@ fun MyApp() {
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
-    MyTheme {
-        MyApp()
+    MySootheTheme {
+        MySootheApp()
     }
 }
 
 @Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+    MySootheTheme(darkTheme = true) {
+        MySootheApp()
     }
 }
