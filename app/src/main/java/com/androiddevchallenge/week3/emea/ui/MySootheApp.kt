43b00ef443fc8356bf0b6a0c0f109f2fd68dead3
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2021-11-20 12:39:51 +0100

    Set MySootheApp as MainActivity content
    Extend MainActivity from ComponentActivity instead of AppCompatActivity.
    Set MainActivity orientation to portrait.
    Set MySootheApp as MainActivity content.
    Move MySootheApp to a new file.

--- app/src/main/java/com/androiddevchallenge/week3/emea/ui/MySootheApp.kt
+++ app/src/main/java/com/androiddevchallenge/week3/emea/ui/MySootheApp.kt
@@ -15,14 +15,10 @@
  */
 package com.androiddevchallenge.week3.emea.ui
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import com.androiddevchallenge.week3.emea.ui.theme.MySootheTheme
 
-class MainActivity : ComponentActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent { MySootheApp() }
-    }
+@Composable
+fun MySootheApp() {
+    MySootheTheme {}
 }
