63ba683434fac55b4f72c816cc04d1005171e467
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2021-11-21 12:31:16 +0100

    Set the status bar transparent
    Set the decor view to not fit the content view to the insets.
    Add the Accompanist System UI Controller dependency.
    Set all of the system bar colors to be transparent, and use dark icons
    if we're in light theme.

--- app/src/main/java/com/androiddevchallenge/week3/emea/ui/MainActivity.kt
+++ app/src/main/java/com/androiddevchallenge/week3/emea/ui/MainActivity.kt
@@ -18,11 +18,16 @@ package com.androiddevchallenge.week3.emea.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // This app draws behind the system bars, so we want to handle fitting system windows
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent { MySootheApp() }
     }
 }
--- app/src/main/java/com/androiddevchallenge/week3/emea/ui/MySootheApp.kt
+++ app/src/main/java/com/androiddevchallenge/week3/emea/ui/MySootheApp.kt
@@ -15,10 +15,23 @@
  */
 package com.androiddevchallenge.week3.emea.ui
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import com.androiddevchallenge.week3.emea.ui.theme.MySootheTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 fun MySootheApp() {
-    MySootheTheme {}
+    MySootheTheme {
+        val systemUiController = rememberSystemUiController()
+        val useDarkIcons = MaterialTheme.colors.isLight
+
+        // Update all of the system bar colors to be transparent, and use
+        // dark icons if we're in light theme
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = useDarkIcons
+        )
+    }
 }
