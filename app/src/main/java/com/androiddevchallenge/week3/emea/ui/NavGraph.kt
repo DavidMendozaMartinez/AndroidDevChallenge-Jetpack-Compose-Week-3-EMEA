9b2d7c8686deda7f36c3ad6df0092ad24a52fe00
David Mendoza <david.mendoza.mnez@gmail.com>
2022-07-10 09:54:20 +0200

    Create the Main screen
    Create the Main file.
    Define the Main composable.
    Add the Main composable to the NavHost.

--- app/src/main/java/com/androiddevchallenge/week3/emea/ui/NavGraph.kt
+++ app/src/main/java/com/androiddevchallenge/week3/emea/ui/NavGraph.kt
@@ -21,6 +21,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.androiddevchallenge.week3.emea.ui.screen.login.LogIn
+import com.androiddevchallenge.week3.emea.ui.screen.main.Main
 import com.androiddevchallenge.week3.emea.ui.screen.welcome.Welcome
 
 /**
@@ -53,6 +54,6 @@ fun NavGraph(
                 onSignUpClicked = {},
             )
         }
-        composable(Destinations.MAIN.route) {}
+        composable(Destinations.MAIN.route) { Main() }
     }
 }
