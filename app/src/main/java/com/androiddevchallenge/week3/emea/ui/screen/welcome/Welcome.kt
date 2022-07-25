6b3853693d997065a3b15f018bf8e978e7c920f1
David Mendoza <david.mendoza.mnez@gmail.com>
2022-07-25 13:03:31 +0200

    Add the components to the Welcome screen
    Add the detailed components in the design to the Welcome screen.

--- app/src/main/java/com/androiddevchallenge/week3/emea/ui/screen/welcome/Welcome.kt
+++ app/src/main/java/com/androiddevchallenge/week3/emea/ui/screen/welcome/Welcome.kt
@@ -17,12 +17,92 @@ package com.androiddevchallenge.week3.emea.ui.screen.welcome
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.androiddevchallenge.week3.emea.R
 import com.androiddevchallenge.week3.emea.ui.theme.MySootheTheme
 
 @Composable
 fun Welcome() {
+    Box(modifier = Modifier.background(color = MaterialTheme.colors.background)) {
+        Image(
+            painter = painterResource(id = R.drawable.bg_welcome),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logotype),
+                contentDescription = stringResource(id = R.string.app_name)
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(72.dp),
+                shape = MaterialTheme.shapes.medium,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.primary
+                )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.welcome_button_sign_up).uppercase(),
+                    color = MaterialTheme.colors.onPrimary,
+                    style = MaterialTheme.typography.button
+                )
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(72.dp),
+                shape = MaterialTheme.shapes.medium,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.secondary
+                )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.welcome_button_log_in).uppercase(),
+                    color = MaterialTheme.colors.onSecondary,
+                    style = MaterialTheme.typography.button
+                )
+            }
+        }
+    }
 }
 
 @Preview(name = "Light Theme", widthDp = 360, heightDp = 640, uiMode = UI_MODE_NIGHT_NO)
